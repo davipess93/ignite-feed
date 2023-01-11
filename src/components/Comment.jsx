0e5cc8fe1d7bd225182f499fe37fbce8c8890e76
Davi Pessoa <davialvespessoa@gmail.com>
2023-01-11 17:50:48 -0300

    Comunicação entre componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,12 @@ import { Avatar } from './Avatar';
 
 import sytles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return(
     <div className={sytles.comment}>
       <Avatar hasBorder={false} src="https://github.com/maykbrito.png" />
@@ -16,7 +21,7 @@ export function Comment({ content }) {
               <time title="11 de maio às 08:13h" dateTime="2022-11-09 16:49:21">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
